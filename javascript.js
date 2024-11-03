51bdf06f6baeef3e601d5d9cf593ba94c54304cc
Daveian <[dguxdesigns@gmail.com]>
2024-11-02 22:00:19 -0400

    Add operate function to calculate values

--- javascript.js
+++ javascript.js
@@ -1,3 +1,39 @@
+// let storedValue = null;
+const display = document.querySelector('.display');
+// Variables to store operands, operator, and the display value
+// let currentOperand = "";
+// let previousOperand = "";
+// let currentOperator = null;
+// let shouldResetDisplay = false; // To clear display after an operation
+
+function appendToDisplay(input) {
+    display.value += input;
+};
+
+function clearDisplay() {
+    display.value = ""
+};
+
+function calculate() {
+    const expression = display.value;
+    const operatorMatch = expression.match(/[\+\-\*\/]/); // Find the first operator in the string
+
+    if (operatorMatch) {
+        const operator = operatorMatch[0];
+        const [value1, value2] = expression.split(operator).map(Number); // Split by operator and convert to numbers
+
+        if (!isNaN(value1) && !isNaN(value2)) {
+            display.value = operate(value1, operator, value2);
+        } else {
+            display.value = "Error"; // Handle invalid inputs
+        }
+    } else {
+        display.value = "Error"; // Handle case where no operator is found
+    };
+};
+
+
+//Basic operations
 function add(num1, num2) {
     return num1 + num2;
 };
@@ -17,17 +53,19 @@ function divide(num1, num2) {
     return num1 / num2;
 };
 
-function operate(operator, value1, value2) {
-    switch (operator) {
+
+//operate function
+function operate(value1, operator, value2) {
+    switch(operator) {
         case '+':
             return add(value1, value2);
         case '-':
-            return subtract(value1, value2);
+            return subtract(value1,value2);
         case '*':
             return multiply(value1, value2);
-        case '/':
+        case '/': 
             return divide(value1, value2);
         default:
-            return 0;
+            return "Error";
     };
 };
